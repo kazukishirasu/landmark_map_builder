3cf15431286c892145a50761e1cc8d4f4c5f4ece
kazukishirasu <s19c1057fa@s.chibakoudai.jp>
2024-09-13 15:32:01 +0900

    add dbscan

---
+++ include/landmark_map_builder/dbscan.h
@@ -0,0 +1,15 @@
+#ifndef DBSCAN_H_
+#define DBSCAN_H_
+
+namespace landmark_map_builder {
+
+class dbscan
+{
+private:
+public:
+    dbscan();
+    ~dbscan();
+};
+
+}
+#endif
---
+++ src/dbscan.cpp
@@ -0,0 +1,20 @@
+#include "landmark_map_builder/dbscan.h"
+
+namespace landmark_map_builder
+{
+
+dbscan::dbscan()
+{
+}
+
+dbscan::~dbscan()
+{
+}
+
+}
+
+int main(int argc, char **argv)
+{
+    landmark_map_builder::dbscan dbscan;
+    return 0;
+}
